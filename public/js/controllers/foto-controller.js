7135b79489053eb80bf4d4ee3ba8928904a759db
Tiago Lima <tiago.jlima@yahoo.com.br>
2015-07-20 14:51:19 -0300

    cadastro de imagens init

---
+++ public/js/controllers/foto-controller.js
@@ -0,0 +1,36 @@
+angular.module('alurapic').controller('FotoController', function($scope, cadastroDeFotos, recursoFoto, $routeParams){
+
+	$scope.foto = {};
+	$scope.mensagem = '';
+
+	if($routeParams.fotoId){
+
+		recursoFoto.get({fotoId: $routeParams.fotoId}, function(foto){
+			$scope.foto = foto;
+		}, function(erro){
+			console.log(erro);
+			$scope.mensagem = 'Não foi possível obter a foto';
+		});
+	}
+
+	$scope.submeter = function(){
+		if($scope.formulario.$valid){
+			
+			cadastroDeFotos.cadastrar($scope.foto)
+			.then(function(dados){
+				$scope.mensagem = dados.mensagem;
+				if(dados.inclusao){
+					$scope.foto = {};
+				}
+				// $scope.$broadcast('fotoCadastrada');
+			})
+			.catch(function(dados){
+				$scope.mensagem = dados.mensagem; 
+			});
+
+			
+		}
+	};
+});
+
+// http://www.fundosanimais.com/Minis/leoes.jpg
